e5730de9ffd3d7870f073e5539a3b34f4f45ff7b
Raquel Mena <raquelmena03@gmail.com>
2021-08-04 14:38:38 -0500

    Initial Commit

---
+++ ATXBS App/ATXBS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATXBS_AppApp.swift
+//  ATXBS App
+//
+//  Created by Ada on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ATXBS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ATXBS App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ATXBS App
+//
+//  Created by Ada on 8/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
