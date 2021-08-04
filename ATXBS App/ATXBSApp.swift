5f39de62ef8e0f0fc4e2a0b3731bc43f51d57399
Raquel Mena <raquelmena03@gmail.com>
2021-08-04 14:44:43 -0500

    set up folders for views, models, view models renamed default view to launch view

--- ATXBS App/ATXBSApp.swift
+++ ATXBS App/ATXBSApp.swift
@@ -1,5 +1,5 @@
 //
-//  ATXBS_AppApp.swift
+//  ATXBSApp.swift
 //  ATXBS App
 //
 //  Created by Ada on 8/4/21.
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct ATXBS_AppApp: App {
+struct ATXBSApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
