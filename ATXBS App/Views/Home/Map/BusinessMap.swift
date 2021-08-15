7335c5027859ef3652248f346eadc2fae5ce7254
Raquel Mena <raquelmena03@gmail.com>
2021-08-14 21:08:54 -0500

    updated map view for geocoding

--- ATXBS App/Views/Home/Map/BusinessMap.swift
+++ ATXBS App/Views/Home/Map/BusinessMap.swift
@@ -30,15 +30,17 @@ struct BusinessMap: UIViewRepresentable {
     }
     
     func makeUIView(context: Context) -> MKMapView {
-        let mapView = MKMapView()
-        //make user show up on map
-        mapView.showsUserLocation = true
-        mapView.userTrackingMode = .followWithHeading
-        
-        //set region
-        
-        return mapView
-    }
+       let mapView = MKMapView()
+       //make user show up on map
+       mapView.showsUserLocation = true
+       mapView.userTrackingMode = .followWithHeading
+       DispatchQueue.main.async{
+           //loop through calling add antoation get coordinate
+       mapView.addAnnotation(getCoordinate(addressString: "Oakland"))
+       }
+     
+       return mapView
+   }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
         //remove all annotations
@@ -52,4 +54,28 @@ struct BusinessMap: UIViewRepresentable {
     static func dismantleUIView(_ uiView: MKMapView, coordinator: ()) {
         
     }
+    func getCoordinate( addressString : String) -> MKPointAnnotation {
+        
+        let geocoder = CLGeocoder()
+        var annotations2 = [CLPlacemark]()
+        var a = MKPointAnnotation()
+        DispatchQueue.main.async {
+            geocoder.geocodeAddressString("Oakland") { (placemarks, error) in
+            if error == nil {
+                if (placemarks?[0]) != nil {
+                    var location1 = placemarks! as [CLPlacemark]
+                    print(location1)
+                    var lat = location1[0].location?.coordinate.latitude
+                    var long = location1[0].location?.coordinate.longitude
+        
+                    a.coordinate = CLLocationCoordinate2D(latitude: lat!, longitude: long!)
+                    a.title = "TEST"
+                }
+            }
+            print(error ?? "")
+            }
+        }
+        return a
+        }
+    
 }
